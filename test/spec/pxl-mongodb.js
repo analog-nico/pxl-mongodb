11bc47c4b28fa030869f761a84ebf6e94989f35c
analog-nico <nicolai.kamenzky@testrails.org>
2016-10-06 00:38:19 -0700

    test: basic functionality

---
+++ test/spec/pxl-mongodb.js
@@ -0,0 +1,116 @@
+'use strict'
+
+let PxlMongodb = require('../../')
+
+describe('PxlMongodb', () => {
+
+    let pxl = null
+
+    before(() => {
+
+        pxl = new PxlMongodb({
+            collectionPxls: 'pxl-mongodb-text-pxls',
+            collectionLinks: 'pxl-mongodb-text-links'
+        })
+
+        return pxl.connect()
+
+    })
+
+    after(() => {
+
+        return pxl.disconnect()
+
+    })
+
+    it('creates and logs a pxl', () => {
+
+        let user = String(Math.random())
+        let pxlKey = null
+
+        return pxl.createPxl({ user })
+            .then((doc) => {
+
+                expect(doc.user).to.eql(user)
+                expect(doc.count).to.eql(0)
+
+                pxlKey = doc.pxl
+
+                return pxl.logPxl(pxlKey)
+
+            })
+            .then(() => {
+
+                return pxl.persistenceLayer.db.collection('pxl-mongodb-text-pxls').findOne({ pxl: pxlKey })
+                    .then((doc) => {
+
+                        expect(doc.user).to.eql(user)
+                        expect(doc.count).to.eql(1)
+
+                    })
+
+            })
+            .then(() => {
+
+                return pxl.logPxl(pxlKey)
+
+            })
+            .then(() => {
+
+                return pxl.persistenceLayer.db.collection('pxl-mongodb-text-pxls').findOne({ pxl: pxlKey })
+                    .then((doc) => {
+
+                        expect(doc.user).to.eql(user)
+                        expect(doc.count).to.eql(2)
+
+                    })
+
+            })
+
+    })
+
+    it('does not log a not existing pxl', () => {
+
+        return pxl.logPxl('impossible pxl')
+            .then(() => {
+                throw new Error('Expected error')
+            })
+            .catch((err) => {
+                expect(err.message).to.eql('Pxl not found.')
+            })
+
+    })
+
+    it('shortens and unshortens a link', () => {
+
+        let link = String(Math.random())
+
+        return pxl.shorten(link)
+            .then((doc) => {
+
+                expect(doc.link).to.eql(link)
+
+                return pxl.unshorten(doc.linkId)
+
+            })
+            .then((doc) => {
+
+                expect(doc.link).to.eql(link)
+
+            })
+
+    })
+
+    it('does not unshorten a not existing link', () => {
+
+        return pxl.unshorten('impossible link')
+            .then(() => {
+                throw new Error('Expected error')
+            })
+            .catch((err) => {
+                expect(err.message).to.eql('Link not found.')
+            })
+
+    })
+
+})
